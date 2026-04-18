316e3d012f317650e61e0f5572fafc8223bf389b
ignacrack <uni.ignaciosantillan55@gmail.com>
2026-04-18 18:14:04 -0300

    Update ProyectoPrueba.java
    hola
    
    Co-Authored-By: Nau <186977766+Naux12@users.noreply.github.com>

--- src/main/java/com/mycompany/proyectoprueba/ProyectoPrueba.java
+++ src/main/java/com/mycompany/proyectoprueba/ProyectoPrueba.java
@@ -11,6 +11,6 @@ package com.mycompany.proyectoprueba;
 public class ProyectoPrueba {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("hola");
     }
 }
