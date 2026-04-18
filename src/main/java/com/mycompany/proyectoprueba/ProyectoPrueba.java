db4454654ff96537dde717bfe48cc55bb9cf46d2
Nau <nahuelsshitbox@gmail.com>
2026-04-18 17:51:50 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/proyectoprueba/ProyectoPrueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoprueba;
+
+/**
+ *
+ * @author NAU
+ */
+public class ProyectoPrueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
